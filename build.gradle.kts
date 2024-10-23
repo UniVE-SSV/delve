13d3224d1fe387c78adb1b582d8597d03b1b2d67
Giacomo Zanatta <giacomo.zanatta@unive.it>
2024-10-23 10:09:47 +0200

    [GRADLE] Update dependencies
    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "it.unive"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -19,7 +19,7 @@ dependencies {
     antlr("org.antlr:antlr4:4.8-1")
 
     // utils
-    api("commons-io:commons-io:2.8.0")
+    api("commons-io:commons-io:2.14.0")
     api("org.apache.commons:commons-lang3:3.9")
     api("org.apache.commons:commons-text:1.10.0")
     api("org.apache.commons:commons-collections4:4.4")
@@ -38,11 +38,11 @@ dependencies {
     implementation("guru.nidi:graphviz-java:0.18.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("org.apache.httpcomponents:httpclient:4.5.14")
-    implementation("org.springframework:spring-web:6.1.6")
+    implementation("org.springframework:spring-web:6.1.12")
     implementation("org.jdom:jdom2:2.0.5")
     implementation("com.sun.xml.bind:jaxb-impl:3.0.2")
     implementation("org.thymeleaf:thymeleaf:3.0.15.RELEASE")
-    implementation("com.google.code.gson:gson:2.8.7")
+    implementation("com.google.code.gson:gson:2.8.9")
     implementation("io.github.classgraph:classgraph:4.8.175")
 
     api("org.graphstream:gs-core:2.0")
@@ -53,7 +53,7 @@ dependencies {
     // reflective lookup
     api("org.reflections:reflections:0.9.12")
 
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.1")
     testImplementation("org.thymeleaf:thymeleaf:3.0.15.RELEASE")
 
     testImplementation("nl.jqno.equalsverifier:equalsverifier:3.6")
