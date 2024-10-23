63fe0b5cb47a3de3f50f35c1dc2a99845698365b
Giacomo Zanatta <giacomo.zanatta@unive.it>
2024-10-23 11:03:03 +0200

    [MAIN] Add a minimal main for testing
    Add a minimal main for testing

--- src/main/java/it/unive/delve/Main.java
+++ src/main/java/it/unive/delve/Main.java
@@ -1,12 +1,48 @@
 package it.unive.delve;
 
+import it.unive.golisa.frontend.GoFrontEnd;
 import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.SimpleAbstractState;
+import it.unive.lisa.analysis.heap.pointbased.FieldSensitivePointBasedHeap;
+import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
+import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
+import it.unive.lisa.analysis.types.InferredTypes;
+import it.unive.lisa.interprocedural.ReturnTopPolicy;
+import it.unive.lisa.interprocedural.callgraph.RTACallGraph;
+import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
+import it.unive.lisa.program.Program;
 import it.unive.lisa.conf.LiSAConfiguration;
+import it.unive.pylisa.PyFrontend;
+import it.unive.pylisa.PyLiSA;
+import it.unive.ros.lisa.analysis.constants.ConstantPropagation;
+
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        LiSA lisa = new LiSA(new LiSAConfiguration());
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        LiSAConfiguration conf = new LiSAConfiguration();
+        conf.workdir = "output";
+        conf.serializeResults = false;
+        conf.jsonOutput = false;
+        conf.analysisGraphs = LiSAConfiguration.GraphType.HTML_WITH_SUBNODES;
+        conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
+        conf.callGraph = new RTACallGraph();
+        conf.openCallPolicy = ReturnTopPolicy.INSTANCE;
+        conf.optimize = false;
+        //conf.semanticChecks.add(rosGraphDumper);
+        FieldSensitivePointBasedHeap heap = new FieldSensitivePointBasedHeap().bottom();
+        TypeEnvironment<InferredTypes> type = new TypeEnvironment<>(new InferredTypes());
+        ValueEnvironment<ConstantPropagation> domain = new ValueEnvironment<>(new ConstantPropagation());
+        conf.abstractState = new SimpleAbstractState<>(heap, domain, type);
+        Program goProgram = GoFrontEnd.processFile("tests/gin/gin.go");
+
+        PyFrontend translator = new PyFrontend("tests/fastapi/microserviceA.py", false);
+        Program program = translator.toLiSAProgram();
+
+        LiSA lisa = new LiSA(conf);
+        lisa.run(program);
+        lisa.run(goProgram);
     }
 }
